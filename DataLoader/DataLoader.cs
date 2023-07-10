b9a29746bbf86230d125f2ecc0facd2163707ff7
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-07-10 13:36:36 +0200

    add initial project

---
+++ DataLoader/DataLoader.cs
@@ -0,0 +1,157 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FHOOE_IMP.MS_Annika.Utils.NonCleavableSearch
+{
+    public class DataLoader
+    {
+        const int MASS_RANGE = 1300;
+        const int MASS_MULTIPLIER = 100;
+        const int ENCODING_SIZE = MASS_RANGE * MASS_MULTIPLIER;
+
+        const string dll = "VectorSearch.dll";
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr findTopCandidates(IntPtr cV, IntPtr cI, IntPtr sV, IntPtr sI,
+                                                       int cVL, int cIL, int sVL, int sIL,
+                                                       int n, float tolerance);
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int releaseMemory(IntPtr result);
+
+        public static void Main(string[] args)
+        {
+            var nrCandidates = 5000000;
+            var nrSpectra = 100;
+            var topN = 20;
+            var r = new Random();
+
+            //
+            if (args.Length > 0)
+            {
+                nrCandidates = int.Parse(args[0]);
+            }
+
+            if (args.Length > 1)
+            {
+                nrSpectra = int.Parse(args[1]);
+            }
+
+            if (args.Length > 2)
+            {
+                topN = int.Parse(args[2]);
+            }
+
+            //
+            var candidateValues = new int[nrCandidates * 100];
+            var candidatesIdx = new int[nrCandidates];
+            var currentIdx = 0;
+            for (int i = 0; i < candidateValues.Length; i+=100)
+            {
+                candidatesIdx[currentIdx] = i;
+                var tmpValues = new int[100];
+                for (int j = 0; j < tmpValues.Length; j++)
+                {
+                    var val = r.Next(ENCODING_SIZE);
+                    while (Array.Exists(tmpValues, x => x == val))
+                    {
+                        val = r.Next(ENCODING_SIZE);
+                    }
+                    tmpValues[j] = val;
+                }
+                Array.Sort(tmpValues);
+                for (int j = 0; j < tmpValues.Length; j++)
+                {
+                    candidateValues[i+j] = tmpValues[j];
+                }
+                currentIdx++;
+                if (currentIdx % 5000 == 0)
+                {
+                    Console.WriteLine($"Generated {currentIdx} candidates...");
+                }
+            }
+
+            //
+            var spectraValues = new int[nrSpectra * 500];
+            var spectraIdx = new int[nrSpectra];
+            currentIdx = 0;
+            for (int i = 0; i < spectraValues.Length; i+=500)
+            {
+                spectraIdx[currentIdx] = i;
+                var tmpValues = new int[500];
+                for (int j = 0; j < tmpValues.Length; j++)
+                {
+                    var val = r.Next(ENCODING_SIZE);
+                    while (Array.Exists(tmpValues, x => x == val))
+                    {
+                        val = r.Next(ENCODING_SIZE);
+                    }
+                    tmpValues[j] = val;
+                }
+                Array.Sort(tmpValues);
+                for (int j = 0; j < tmpValues.Length; j++)
+                {
+                    spectraValues[i + j] = tmpValues[j];
+                }
+                currentIdx++;
+            }
+
+            //
+            var sw = Stopwatch.StartNew();
+
+            //
+            var cValuesLoc = GCHandle.Alloc(candidateValues, GCHandleType.Pinned);
+            var cIdxLoc = GCHandle.Alloc(candidatesIdx, GCHandleType.Pinned);
+            var sValuesLoc = GCHandle.Alloc(spectraValues, GCHandleType.Pinned);
+            var sIdxLoc = GCHandle.Alloc(spectraIdx, GCHandleType.Pinned);
+            var resultArray = new int[spectraIdx.Length * topN];
+            var memStat = 1;
+            try
+            {
+                IntPtr cValuesPtr = cValuesLoc.AddrOfPinnedObject();
+                IntPtr cIdxPtr = cIdxLoc.AddrOfPinnedObject();
+                IntPtr sValuesPtr = sValuesLoc.AddrOfPinnedObject();
+                IntPtr sIdxPtr = sIdxLoc.AddrOfPinnedObject();
+
+                IntPtr result = findTopCandidates(cValuesPtr, cIdxPtr, sValuesPtr, sIdxPtr,
+                                                  candidateValues.Length, candidatesIdx.Length, spectraValues.Length, spectraIdx.Length,
+                                                  topN, (float) 0.02);
+
+                Marshal.Copy(result, resultArray, 0, spectraIdx.Length * topN);
+
+                memStat = releaseMemory(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong:");
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (cValuesLoc.IsAllocated) { cValuesLoc.Free(); }
+                if (cIdxLoc.IsAllocated) { cIdxLoc.Free(); }
+                if (sValuesLoc.IsAllocated) { sValuesLoc.Free(); }
+                if (sIdxLoc.IsAllocated) { sIdxLoc.Free(); }
+            }
+
+            //
+            sw.Stop();
+
+            for (int i = 0; i < topN; i++)
+            {
+                Console.WriteLine(resultArray[i]);
+            }
+
+            Console.WriteLine($"MemStat: {memStat}");
+            Console.WriteLine("Time for candidate search (SpMV):");
+            Console.WriteLine(sw.Elapsed.TotalSeconds.ToString());
+
+            //
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("Done!");
+
+            return;
+        }
+    }
+}
---
+++ VectorSearch/dllmain.cpp
@@ -0,0 +1,173 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <iostream>
+
+#define METHOD_EXPORTS
+#ifdef METHOD_EXPORTS
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif
+
+const int MASS_RANGE = 1300;
+const int MASS_MULTIPLIER = 100;
+const int ENCODING_SIZE = MASS_RANGE * MASS_MULTIPLIER;
+const int APPROX_NNZ_PER_ROW = 100;
+const int MAX_SPECTRA = 100000;
+const int MAX_TOP_N = 100;
+const int RESULT_ARRAY_SIZE = MAX_SPECTRA * MAX_TOP_N;
+const double ONE_OVER_SQRT_PI = 0.39894228040143267793994605993438;
+
+extern "C" {
+    EXPORT int* findTopCandidates(int*, int*, int*, int*, 
+                                  int, int, int, int, 
+                                  int, float);
+
+    EXPORT int releaseMemory(int*);
+}
+
+float squared(float);
+float normpdf(float, float, float);
+
+/// <summary>
+/// A function that calculates the top n candidates for each spectrum.
+/// </summary>
+/// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
+/// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
+/// <param name="spectraValues">An integer array of peaks from experimental spectra flattened.</param>
+/// <param name="spectraIdx">An integer array that contains indices of where each spectrum starts in spectraValues.</param>
+/// <param name="cVLength">Length (int) of candidatesValues.</param>
+/// <param name="cILength">Length (int) of candidatesIdx.</param>
+/// <param name="sVLength">Length (int) of spectraValues.</param>
+/// <param name="sILength">Length (int) of spectraIdx.</param>
+/// <param name="n">How many of the best hits should be returned (int).</param>
+/// <param name="tolerance">Tolerance for peak matching (float).</param>
+/// <returns>An integer array of length sILength * n containing the indexes of the top n candidates for each spectrum.</returns>
+int* findTopCandidates(int* candidatesValues, int* candidatesIdx, int* spectraValues, int* spectraIdx, 
+                       int cVLength, int cILength, int sVLength, int sILength,
+                       int n, float tolerance) {
+
+    if (sILength >= MAX_SPECTRA) {
+        throw std::invalid_argument("Provided more spectra than allowed.");
+    }
+    if (n >= MAX_TOP_N) {
+        throw std::invalid_argument("Retrieving more hits per spectrum than allowed.");
+    }
+
+    auto* m = new Eigen::SparseMatrix<float, Eigen::RowMajor>(cILength, ENCODING_SIZE);
+    m->reserve(Eigen::VectorXi::Constant(cILength, APPROX_NNZ_PER_ROW));
+
+    auto* norm = new Eigen::Vector<float, Eigen::Dynamic>(cILength);
+
+    int currentRow = 0;
+    for (int i = 0; i < cILength; ++i) {
+        int nrValues = 0;
+        int startIter = candidatesIdx[i];
+        int endIter = i + 1 == cILength ? cVLength : candidatesIdx[i + 1];
+        for (int j = startIter; j < endIter; ++j) {
+            m->insert(currentRow, candidatesValues[j]) = 1.0;
+            ++nrValues;
+        }
+        norm->coeffRef(currentRow) = 1.0/nrValues;
+        ++currentRow;
+    }
+
+    m->makeCompressed();
+
+    //auto result = new std::vector<int>;
+    //result.reserve(sILength * n);
+    auto* result = new int[RESULT_ARRAY_SIZE];
+    auto t = round(tolerance * MASS_MULTIPLIER);
+
+    for (int i = 0; i < sILength; ++i) {
+        int startIter = spectraIdx[i];
+        int endIter = i + 1 == sILength ? sVLength : spectraIdx[i + 1];
+        auto* v = new Eigen::SparseVector<float, Eigen::ColMajor>(ENCODING_SIZE);
+        for (int j = startIter; j < endIter; ++j) {
+            auto currentPeak = spectraValues[j];
+            auto minPeak = currentPeak - t > 0 ? currentPeak - t : 0;
+            auto maxPeak = currentPeak + t < ENCODING_SIZE ? currentPeak + t : ENCODING_SIZE - 1;
+
+            for (int k = minPeak; k <= maxPeak; ++k) {
+                float currentVal = v->coeffRef(k);
+                float newVal = normpdf((float) k, (float) currentPeak, (float) t / 3);
+                v->coeffRef(k) = max(currentVal, newVal);
+            }
+        }
+        
+        auto* spmv = new Eigen::Vector<float, Eigen::Dynamic>(cILength);
+        *spmv = Eigen::Product(*m, *v);
+
+        auto* normspmv = new Eigen::Vector<float, Eigen::Dynamic>(cILength);
+        *normspmv = spmv->cwiseProduct(*norm);
+
+        for (int j = 0; j < n; ++j) {
+            Eigen::Index max_idx;
+            float max = normspmv->maxCoeff(&max_idx);
+            //result.push_back((int) max_idx);
+            result[i * n + j] = (int) max_idx;
+            normspmv->coeffRef(max_idx) = 0.0;
+        }
+
+        delete normspmv;
+        delete spmv;
+        delete v;
+    }
+
+    delete norm;
+    delete m;
+
+    //return result.data();
+    return result;
+}
+
+/// <summary>
+/// Free memory after result has been marshalled.
+/// </summary>
+/// <param name="result">The result array.</param>
+/// <returns>0</returns>
+int releaseMemory(int* result) {
+
+    delete[] result;
+    return 0;
+}
+
+/// <summary>
+/// Returns the square for a given value x.
+/// </summary>
+/// <param name="x">The value to be squared.</param>
+/// <returns>Square of x.</returns>
+float squared(float x) {
+    return x * x;
+}
+
+/// <summary>
+/// Returns the PDF for a given x for the normal distribution given by mu and sigma.
+/// </summary>
+/// <param name="x">The value for which the PDF should be calculated.</param>
+/// <param name="mu">The mu of the normal distribution.</param>
+/// <param name="sigma">The sigma of the normal distribution.</param>
+/// <returns>The PDF at x for the normal distribution given by mu and sigma</returns>
+float normpdf(float x, float mu, float sigma) {
+    return (ONE_OVER_SQRT_PI / sigma) * exp(-0.5 * squared((x - mu) / sigma));
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
