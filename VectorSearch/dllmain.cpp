7f3f85a1a83c0a263e78f5b6dbcc2597720690d7
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-01-25 15:13:20 +0100

    implement i32 spmv and spmm

--- VectorSearch/dllmain.cpp
+++ VectorSearch/dllmain.cpp
@@ -8,8 +8,8 @@
 #include <iostream>
 
 const int versionMajor = 1;
-const int versionMinor = 5;
-const int versionFix = 1;
+const int versionMinor = 6;
+const int versionFix = 0;
 
 #define METHOD_EXPORTS
 #ifdef METHOD_EXPORTS
@@ -22,6 +22,7 @@ const int MASS_RANGE = 5000;
 const int MASS_MULTIPLIER = 100;
 const int ENCODING_SIZE = MASS_RANGE * MASS_MULTIPLIER;
 const int APPROX_NNZ_PER_ROW = 100;
+const int ROUNDING_ACCURACY = 1000;
 const double ONE_OVER_SQRT_PI = 0.39894228040143267793994605993438;
 
 extern "C" {
@@ -41,6 +42,14 @@ extern "C" {
                                    bool, bool,
                                    int, int);
 
+    EXPORT int* findTopCandidates2Int(int*, int*,
+                                      int*, int*,
+                                      int, int,
+                                      int, int,
+                                      int, float,
+                                      bool, bool,
+                                      int, int);
+
     EXPORT int* findTopCandidatesBatched(int*, int*,
                                          int*, int*,
                                          int, int,
@@ -59,6 +68,15 @@ extern "C" {
                                           int,
                                           int, int);
 
+    EXPORT int* findTopCandidatesBatched2Int(int*, int*,
+                                             int*, int*,
+                                             int, int,
+                                             int, int,
+                                             int, float,
+                                             bool, bool,
+                                             int,
+                                             int, int);
+
     EXPORT int releaseMemory(int*);
 }
 
@@ -66,7 +84,7 @@ float squared(float);
 float normpdf(float, float, float);
 
 /// <summary>
-/// A function that calculates the top n candidates for each spectrum (SpM*SpV). 
+/// A function that calculates the top n candidates for each spectrum (SpM*SpV) using f32 operations. 
 /// </summary>
 /// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
 /// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
@@ -172,7 +190,7 @@ int* findTopCandidates(int* candidatesValues, int* candidatesIdx,
 }
 
 /// <summary>
-/// A function that calculates the top n candidates for each spectrum (SpM*V). 
+/// A function that calculates the top n candidates for each spectrum (SpM*V) using f32 operations. 
 /// </summary>
 /// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
 /// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
@@ -278,7 +296,117 @@ int* findTopCandidates2(int* candidatesValues, int* candidatesIdx,
 }
 
 /// <summary>
-/// A function that calculates the top n candidates for each spectrum (SpM*SpM).
+/// A function that calculates the top n candidates for each spectrum (SpM*V) using i32 operations. 
+/// </summary>
+/// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
+/// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
+/// <param name="spectraValues">An integer array of peaks from experimental spectra flattened.</param>
+/// <param name="spectraIdx">An integer array that contains indices of where each spectrum starts in spectraValues.</param>
+/// <param name="cVLength">Length (int) of candidatesValues.</param>
+/// <param name="cILength">Length (int) of candidatesIdx.</param>
+/// <param name="sVLength">Length (int) of spectraValues.</param>
+/// <param name="sILength">Length (int) of spectraIdx.</param>
+/// <param name="n">How many of the best hits should be returned (int).</param>
+/// <param name="tolerance">Tolerance for peak matching (float >= 0.01).</param>
+/// <param name="normalize">If candidate vectors should be normalized to sum(elements) = 1 (bool).</param>
+/// <param name="gaussianTol">If spectrum peaks should be modelled as normal distributions or not (bool).</param>
+/// <param name="cores">Number of cores (int) used by Eigen.</param>
+/// <param name="verbose">Print info every (int) processed spectra.</param>
+/// <returns>An integer array of length sILength * n containing the indexes of the top n candidates for each spectrum.</returns>
+int* findTopCandidates2Int(int* candidatesValues, int* candidatesIdx,
+                           int* spectraValues, int* spectraIdx,
+                           int cVLength, int cILength,
+                           int sVLength, int sILength,
+                           int n, float tolerance,
+                           bool normalize, bool gaussianTol,
+                           int cores, int verbose) {
+
+    if (tolerance < 0.01f) {
+        throw std::invalid_argument("Tolerance must not be smaller than 0.01 for i32 operations!");
+    }
+
+    int usedCores = 0;
+    Eigen::setNbThreads(cores);
+    usedCores = Eigen::nbThreads();
+
+    std::cout << "Running Eigen dense vector search version " << versionMajor << "." << versionMinor << "." << versionFix << std::endl;
+    std::cout << "Using " << usedCores << " threads in total." << std::endl;
+
+    auto* m = new Eigen::SparseMatrix<int, Eigen::RowMajor>(cILength, ENCODING_SIZE);
+    m->reserve(Eigen::VectorXi::Constant(cILength, APPROX_NNZ_PER_ROW));
+
+    int currentRow = 0;
+    for (int i = 0; i < cILength; ++i) {
+        int startIter = candidatesIdx[i];
+        int endIter = i + 1 == cILength ? cVLength : candidatesIdx[i + 1];
+        int nrNonZero = endIter - startIter;
+        float val = normalize ? 1.0 / (float) nrNonZero : 1.0;
+        for (int j = startIter; j < endIter; ++j) {
+            m->insert(currentRow, candidatesValues[j]) = (int) round(val * (float) ROUNDING_ACCURACY);
+        }
+        ++currentRow;
+    }
+
+    m->makeCompressed();
+
+    auto* result = new int[sILength * n];
+    float t = round(tolerance * MASS_MULTIPLIER);
+
+    for (int i = 0; i < sILength; ++i) {
+        int startIter = spectraIdx[i];
+        int endIter = i + 1 == sILength ? sVLength : spectraIdx[i + 1];
+        auto* v = new Eigen::Vector<int, Eigen::Dynamic>(ENCODING_SIZE);
+        v->setZero();
+        for (int j = startIter; j < endIter; ++j) {
+            auto currentPeak = spectraValues[j];
+            auto minPeak = currentPeak - t > 0 ? currentPeak - t : 0;
+            auto maxPeak = currentPeak + t < ENCODING_SIZE ? currentPeak + t : ENCODING_SIZE - 1;
+
+            for (int k = minPeak; k <= maxPeak; ++k) {
+                float currentVal = v->coeffRef(k);
+                float newVal = gaussianTol ? normpdf((float) k, (float) currentPeak, (float) (t / 3.0)) : 1.0;
+                v->coeffRef(k) = (int) round(max(currentVal, newVal) * (float) ROUNDING_ACCURACY);
+            }
+        }
+
+        auto* spmv = new Eigen::Vector<int, Eigen::Dynamic>(cILength);
+        *spmv = Eigen::Product(*m, *v);
+
+        std::vector<int> colValues;
+        colValues.resize(spmv->size());
+        Eigen::Map<Eigen::Vector<int, Eigen::Dynamic>>(colValues.data(), spmv->size()) = *spmv;
+
+        auto* idx = new int[cILength];
+        std::iota(idx, idx + cILength, 0);
+        std::sort(idx, idx + cILength, [&](int i, int j) {return colValues[i] > colValues[j];});
+
+        for (int j = 0; j < n; ++j) {
+            result[i * n + j] = idx[j];
+        }
+
+        delete[] idx;
+
+        spmv->resize(0);
+        v->resize(0);
+        delete spmv;
+        delete v;
+        spmv = NULL;
+        v = NULL;
+
+        if (verbose != 0 && (i + 1) % verbose == 0) {
+            std::cout << "Searched " << i + 1 << " spectra in total..." << std::endl;
+        }
+    }
+
+    m->resize(0, 0);
+    delete m;
+    m = NULL;
+
+    return result;
+}
+
+/// <summary>
+/// A function that calculates the top n candidates for each spectrum (SpM*SpM) using f32 operations.
 /// </summary>
 /// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
 /// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
@@ -413,7 +541,7 @@ int* findTopCandidatesBatched(int* candidatesValues, int* candidatesIdx,
 }
 
 /// <summary>
-/// A function that calculates the top n candidates for each spectrum (SpM*M).
+/// A function that calculates the top n candidates for each spectrum (SpM*M) using f32 operations.
 /// </summary>
 /// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
 /// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
@@ -505,6 +633,134 @@ int* findTopCandidatesBatched2(int* candidatesValues, int* candidatesIdx,
             std::vector<float> colValues;
             colValues.resize(spmM->rows());
             Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>>(colValues.data(), spmM->rows(), 1) = spmM->col(s);
+
+            auto* idx = new int[cILength];
+            std::iota(idx, idx + cILength, 0);
+            std::sort(idx, idx + cILength, [&](int i, int j) {return colValues[i] > colValues[j];});
+
+            for (int j = 0; j < n; ++j) {
+                result[(i + s) * n + j] = idx[j];
+            }
+
+            delete[] idx;
+        }
+
+        spmM->resize(0, 0);
+        delete spmM;
+        spmM = NULL;
+        M->resize(0, 0);
+        delete M;
+        M = NULL;
+
+        if (verbose != 0 && (i + batchSize) % verbose == 0) {
+            std::cout << "Searched " << i + batchSize << " spectra in total..." << std::endl;
+        }
+    }
+
+    m->resize(0, 0);
+    delete m;
+    m = NULL;
+
+    return result;
+}
+
+/// <summary>
+/// A function that calculates the top n candidates for each spectrum (SpM*M) using i32 operations.
+/// </summary>
+/// <param name="candidatesValues">An integer array of theoretical ion masses for all candidates flattened.</param>
+/// <param name="candidatesIdx">An integer array that contains indices of where each candidate starts in candidatesValues.</param>
+/// <param name="spectraValues">An integer array of peaks from experimental spectra flattened.</param>
+/// <param name="spectraIdx">An integer array that contains indices of where each spectrum starts in spectraValues.</param>
+/// <param name="cVLength">Length (int) of candidatesValues.</param>
+/// <param name="cILength">Length (int) of candidatesIdx.</param>
+/// <param name="sVLength">Length (int) of spectraValues.</param>
+/// <param name="sILength">Length (int) of spectraIdx.</param>
+/// <param name="n">How many of the best hits should be returned (int).</param>
+/// <param name="tolerance">Tolerance for peak matching (float >= 0.01).</param>
+/// <param name="normalize">If candidate vectors should be normalized to sum(elements) = 1 (bool).</param>
+/// <param name="gaussianTol">If spectrum peaks should be modelled as normal distributions or not (bool).</param>
+/// <param name="batchSize">How many spectra (int) should be searched at once.</param>
+/// <param name="cores">Number of cores (int) used by Eigen.</param>
+/// <param name="verbose">Print info every (int) processed spectra.</param>
+/// <returns>An integer array of length sILength * n containing the indexes of the top n candidates for each spectrum.</returns>
+int* findTopCandidatesBatched2Int(int* candidatesValues, int* candidatesIdx,
+                                  int* spectraValues, int* spectraIdx,
+                                  int cVLength, int cILength,
+                                  int sVLength, int sILength,
+                                  int n, float tolerance,
+                                  bool normalize, bool gaussianTol,
+                                  int batchSize,
+                                  int cores, int verbose) {
+
+    if (tolerance < 0.01f) {
+        throw std::invalid_argument("Tolerance must not be smaller than 0.01 for i32 operations!");
+    }
+
+    int usedCores = 0;
+    Eigen::setNbThreads(cores);
+    usedCores = Eigen::nbThreads();
+
+    std::cout << "Running Eigen dense matrix search version " << versionMajor << "." << versionMinor << "." << versionFix << std::endl;
+    std::cout << "Using " << usedCores << " threads in total." << std::endl;
+
+    auto* m = new Eigen::SparseMatrix<int, Eigen::RowMajor>(cILength, ENCODING_SIZE);
+    m->reserve(Eigen::VectorXi::Constant(cILength, APPROX_NNZ_PER_ROW));
+
+    int currentRow = 0;
+    for (int i = 0; i < cILength; ++i) {
+        int startIter = candidatesIdx[i];
+        int endIter = i + 1 == cILength ? cVLength : candidatesIdx[i + 1];
+        int nrNonZero = endIter - startIter;
+        float val = normalize ? 1.0 / (float) nrNonZero : 1.0;
+        for (int j = startIter; j < endIter; ++j) {
+            m->insert(currentRow, candidatesValues[j]) = (int) round(val * (float) ROUNDING_ACCURACY);
+        }
+        ++currentRow;
+    }
+
+    m->makeCompressed();
+
+    auto* result = new int[sILength * n];
+    float t = round(tolerance * MASS_MULTIPLIER);
+
+    for (int i = 0; i < sILength; i += batchSize) {
+
+        auto* M = new Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>(ENCODING_SIZE, batchSize);
+        M->setZero();
+
+        for (int s = 0; s < batchSize; ++s) {
+
+            if (i + s >= sILength) {
+                break;
+            }
+
+            int startIter = spectraIdx[i + s];
+            int endIter = i + s + 1 == sILength ? sVLength : spectraIdx[i + s + 1];
+            for (int j = startIter; j < endIter; ++j) {
+                auto currentPeak = spectraValues[j];
+                auto minPeak = currentPeak - t > 0 ? currentPeak - t : 0;
+                auto maxPeak = currentPeak + t < ENCODING_SIZE ? currentPeak + t : ENCODING_SIZE - 1;
+
+                for (int k = minPeak; k <= maxPeak; ++k) {
+                    float currentVal = M->coeff(k, s);
+                    float newVal = gaussianTol ? normpdf((float) k, (float) currentPeak, (float) (t / 3.0)) : 1.0;
+                    M->coeffRef(k, s) = (int) round(max(currentVal, newVal) * (float) ROUNDING_ACCURACY);
+                }
+            }
+        }
+
+        auto* spmM = new Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>(cILength, batchSize);
+        *spmM = Eigen::Product(*m, *M);
+
+        for (int s = 0; s < batchSize; ++s) {
+
+            if (i + s >= sILength) {
+                break;
+            }
+
+            std::vector<int> colValues;
+            colValues.resize(spmM->rows());
+            Eigen::Map<Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>>(colValues.data(), spmM->rows(), 1) = spmM->col(s);
 
             auto* idx = new int[cILength];
             std::iota(idx, idx + cILength, 0);
